ec06d97feaa2dac5653076bf3607f14bd4cb2f19
ebnid <code@ebnid.com>
2023-09-17 20:36:36 +0600

    deploy

--- app/Http/Livewire/Announce.php
+++ app/Http/Livewire/Announce.php
@@ -3,11 +3,19 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Jenssegers\Agent\Agent;
 
 class Announce extends Component
 {
 
     public $is_mobile_device = true;
+    public $browser;
+
+    public function mount()
+    {
+        $agent = new Agent();
+        $this->browser = $agent->browser();
+    }
 
     public function render()
     {
