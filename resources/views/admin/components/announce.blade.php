e2f134228a31d99dc621ad67d18d09c0e5e00a3a
ebnid <code@ebnid.com>
2023-09-18 15:26:55 +0600

    deploy

--- resources/views/admin/components/announce.blade.php
+++ resources/views/admin/components/announce.blade.php
@@ -1,9 +1,7 @@
 <div>
-    @if($is_mobile_device)
-        <div class="bg-black px-4 py-3 text-white mb-5">
-            <p class="text-center text-sm font-medium">
-                <a href="#" class="inline-block underline">You are browsing by {{ $device }} device using {{ $browser }} browser on {{ $platform }} </a>
-            </p>
-        </div>
-    @endif
+    <div class="bg-black px-4 py-3 text-white mb-5 rounded-sm">
+        <p class="text-center text-sm font-medium">
+            <a href="#" class="inline-block underline">You are browsing by {{ $device }} device using {{ $browser }} browser on {{ $platform }} </a>
+        </p>
+    </div>
 </div>
